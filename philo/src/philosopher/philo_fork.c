642ef327ed17094a9a2b952cddb176823d949dc6
tmatis <tmatis@student.42.fr>
2021-07-02 16:04:26 +0200

    norm

--- philo/src/philosopher/philo_fork.c
+++ philo/src/philosopher/philo_fork.c
@@ -6,7 +6,7 @@
 /*   By: tmatis <tmatis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 19:36:09 by tmatis            #+#    #+#             */
-/*   Updated: 2021/07/02 14:01:43 by tmatis           ###   ########.fr       */
+/*   Updated: 2021/07/02 16:04:12 by tmatis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 void	take_forks(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->fork_mutex);
-	printf("%lli %i taken a fork\n", get_relative_time(philo->manager->start_time), philo->philo_id);
+	printf("%lli %i taken a fork\n",
+		get_relative_time(philo->manager->start_time), philo->philo_id);
 	if (philo->manager->config.philo_count > 1)
 	{
 		if (philo->philo_id == philo->manager->config.philo_count)
@@ -25,7 +26,8 @@ void	take_forks(t_philo *philo)
 		else
 			pthread_mutex_lock(
 				&philo->manager->philo_array[philo->philo_id].fork_mutex);
-		printf("%lli %i taken a fork\n",get_relative_time(philo->manager->start_time), philo->philo_id);
+		printf("%lli %i taken a fork\n",
+			get_relative_time(philo->manager->start_time), philo->philo_id);
 	}		
 }
 
